136d35477af02ced32b16d00b57cbf38b604fa09
Fábio Augusto Vitório <44537162+fabio2003@users.noreply.github.com>
2023-04-11 19:26:56 -0300

    Add files via upload

---
+++ Atv3.java
@@ -0,0 +1,28 @@
+/* Nome do Aluno: Fábio Augusto V.
+* RA: 323135349
+* Nome do Programa: Imprime números em ordem crescente
+* Data: 11/04/2023
+*/
+
+import java.util.Scanner;
+
+public class Atv3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Digite um número inteiro: ");
+        int numero = input.nextInt();
+        
+        if (numero % 2 == 0) {
+            System.out.print("O número é par ");
+        } else {
+            System.out.print("O número é ímpar ");
+        }
+        
+        if (numero >= 0) {
+            System.out.print("e positivo.");
+        } else {
+            System.out.print("e negativo.");
+        }
+    }
+}
